ea86313c4d2c0115ba93b7729398b1b4f468badf
Yul Vincent Sullano <yesullano@gmail.com>
2025-10-25 13:47:06 +0800

    add primevue

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import VeeValidate from 'vee-validate';
+import PrimeVue from 'primevue/config';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(VeeValidate)
+app.use(PrimeVue)
 
 app.mount('#app')
