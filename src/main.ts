925526ce2ea929fa406449df22bab57b4f8d429f
Yul Vincent Sullano <yesullano@gmail.com>
2025-10-25 13:46:01 +0800

    add vee-validate

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import VeeValidate from 'vee-validate';
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VeeValidate)
 
 app.mount('#app')
